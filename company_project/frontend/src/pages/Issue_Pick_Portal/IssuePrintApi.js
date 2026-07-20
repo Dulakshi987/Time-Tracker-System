898fe6b8e16e981ab4a99c819b8661eed2d858fe
Dulakshi987 <dulakshikeshani288@gmail.com>
2026-07-20 07:57:01 +0530

    DONE FOR PROPERTIES

--- company_project/frontend/src/pages/Issue_Pick_Portal/IssuePrintApi.js
+++ company_project/frontend/src/pages/Issue_Pick_Portal/IssuePrintApi.js
@@ -2,16 +2,16 @@ import axios from "axios";
 
 const API = "http://localhost:8080/api/pick-issue";
 
-export const startPrint = (data: any) =>
+export const startPrint = (data) =>
   axios.post(`${API}/start`, data);
 
-export const endPrint = (id: number) =>
+export const endPrint = (id) =>
   axios.put(`${API}/end/${id}`);
 
-export const reserveIssue = (data: any) =>
+export const reserveIssue = (data) =>
   axios.post(`${API}/reserve`, data);
 
-export const searchDocs = (docNo: string) =>
+export const searchDocs = (docNo) =>
   axios.get(`${API}/search`, {
     params: { documentNo: docNo }
   });
