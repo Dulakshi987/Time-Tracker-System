4b8d9b12dbdcaf7f1d6ca6a0d7a1667b50f76fff
Dulakshi987 <dulakshikeshani288@gmail.com>
2026-07-20 10:44:43 +0530

    up

--- company_project/backend/src/main/java/com/config/CorsConfig.java
+++ company_project/backend/src/main/java/com/config/CorsConfig.java
@@ -1,19 +1,63 @@
 
+// package com.config;
+
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:5173")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                 .allowedHeaders("*");
+//     }
+// }
+
 package com.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    @Bean
+    public CorsFilter corsFilter() {
 
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*");
+        CorsConfiguration config = new CorsConfiguration();
+
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(Arrays.asList(
+            "https://logitrack-warehouse-system.netlify.app",
+            "http://localhost:5173"
+        ));
+
+        config.setAllowedHeaders(Arrays.asList("*"));
+
+        config.setAllowedMethods(Arrays.asList(
+            "GET",
+            "POST",
+            "PUT",
+            "DELETE",
+            "OPTIONS"
+        ));
+
+        UrlBasedCorsConfigurationSource source =
+                new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
     }
 }
